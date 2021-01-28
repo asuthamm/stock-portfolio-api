357d924fe85000d0ed7ce201816266c201aebbbb
Alex Suthammanont <asuthamm@gmail.com>
2021-01-28 16:40:35 -0500

    create nested api/v1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :watchlists
-  resources :outlooks
-  resources :stocks
-  resources :accounts
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :watchlists
+      resources :outlooks
+      resources :stocks
+      resources :accounts
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
